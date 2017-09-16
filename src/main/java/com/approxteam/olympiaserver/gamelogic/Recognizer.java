91f19b6650a7d28588573e1a23fabace5ad70951
Adam Raźniewski <adam.razniewski@gmail.com>
2017-09-16 19:57:27 +0200

    First look at recognizer logic

---
+++ src/main/java/com/approxteam/olympiaserver/gamelogic/Recognizer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.approxteam.olympiaserver.gamelogic;
+
+import javax.websocket.Session;
+
+/**
+ *
+ * @author adamr
+ */
+public interface Recognizer {
+    public void recognize(Session session, Action action);
+}
